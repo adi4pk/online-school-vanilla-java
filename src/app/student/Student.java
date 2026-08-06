0c602f15b0b14caefa20f7f51548e89cef7edee7
adi4pk <Adrian.stancioiu96@yahoo.com>
2026-08-06 21:20:34 +0300

    db-schema photos

---
+++ src/app/student/Student.java
@@ -0,0 +1,10 @@
+package app.student;
+
+public class Student {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+}
